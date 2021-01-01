496a276583e5f6a40b3b89a72b484d2e484a62c2
Dan Tsir Cohen <dantc12@gmail.com>
2021-01-01 14:57:13 +0200

    fixes in hierarchy
    makes them subsets
    saves as a list of ints (indices)
    fixed packing and covering mistake in article

--- Hierarchy.py
+++ Hierarchy.py
@@ -1,3 +1,4 @@
+import logging
 from copy import copy
 from typing import List, Any
 
@@ -9,7 +10,7 @@ from utils import arr_scaler
 class Hierarchy:
     S: List  # 1-dimensional list
     distances: np.ndarray  # 2-dimensional list of distances
-    hierarchy: List[Any]  # list of lists, the hierarchy
+    hierarchy: List[List[int]]  # list of lists, the hierarchy
     c: float  # covering property
 
     def __init__(self, S: List, distances: np.ndarray, c: float):
@@ -27,10 +28,13 @@ class Hierarchy:
             try:
                 self._build_hierarchy_starting_at(i)
                 break
-            except Exception:
+            except Exception as e:
+                logging.info('Failed {}\'th try at building hierarchy, got {}'.format(str(i + 1), str(self)))
                 self.hierarchy = []
         if len(self.hierarchy) == 0:  # Couldn't build any hierarchies
-            raise Exception("Failed to create a hierarchy, got only {}".format(str(self.hierarchy)))
+            logging.info("Failed to create a hierarchy.")
+        else:
+            logging.info("Created hierarchy successfully: {}".format(str(self)))
 
     def _build_hierarchy_starting_at(self, point_i: int):
         self.hierarchy = []
@@ -41,33 +45,22 @@ class Hierarchy:
 
         i = 1
         while len(p_index_options) > 0:
-            self.hierarchy.append([])
-            # Find first point in layer
-            p_index_options_copy = copy(p_index_options)
-            for p_index in p_index_options_copy:
-                #  checking for the c-covering property: in prev layer it should have a "close" point
-                if self._get_min_distance_from(p_index, self.hierarchy[i - 1]) < self.c * 2 ** -(i-1):
-                    self.hierarchy[i].append(p_index)
-                    _ = p_index_options.pop(p_index_options.index(p_index))
-            if len(self.hierarchy[i]) == 0:  # Didn't find anything to add to hierarchy
-                raise Exception("Failed to create a hierarchy, got only {}".format(str(self.hierarchy)))
-
-            # Fill in more points in layer
+            self.hierarchy.append(copy(self.hierarchy[i-1]))
+            # Fill in points to add to the layer
             p_index_options_copy = copy(p_index_options)
             for p_index in p_index_options_copy:
                 #  verifying the c-covering property and the packing property
-                if self._get_min_distance_from(p_index, self.hierarchy[i - 1]) < self.c * 2 ** -(i-1)\
-                        and self._get_min_distance_from(p_index, self.hierarchy[i]) >= 2 ** -i:
+                if self._get_min_distance_from(p_index, self.hierarchy[i - 1]) <= self.c * 2 ** -(i-1)\
+                        and self._get_min_distance_from(p_index, self.hierarchy[i]) > 2 ** -i:
                     self.hierarchy[i].append(p_index)
                     _ = p_index_options.pop(p_index_options.index(p_index))
+            if len(self.hierarchy[i]) == len(self.hierarchy[i-1]):  # Didn't find anything to add to hierarchy
+                raise Exception("Failed to create a hierarchy, got this far: {}".format(str(self.hierarchy)))
             i += 1
 
     def __str__(self):
-        res = [self.hierarchy[0]]
-        if len(self.hierarchy) > 1:
-            for i in range(1, len(self.hierarchy)):
-                res.append(res[i-1] + self.hierarchy[i])
-        return str(res)
+        return str(self.hierarchy)
+        # return str([[self.S[p_i] for p_i in lvl] for lvl in self.hierarchy])
 
     def _get_min_distance_from(self, p_index, other_points_index: List) -> float:
         t = np.min([self.distances[p_index, j] for j in other_points_index])
