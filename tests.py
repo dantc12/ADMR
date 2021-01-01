674e8e6e9d29636e4c39ecefb6fd1a5ced7fe2f7
Dan Tsir Cohen <dantc12@gmail.com>
2021-01-01 14:57:25 +0200

    changed tests to use `logging`

--- tests.py
+++ tests.py
@@ -1,13 +1,20 @@
+import logging
+
 import numpy as np
 
 from Hierarchy import Hierarchy
 from utils import calc_euclidean_distances, arr_scaler
 
-S = [(0, 0), (1, 0), (2, 0), (3, 0)]
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+logging.info('Entered debug mode.')
+
+S = [(1, 1), (1, -1), (2, 1), (3, 0)]
 distances = calc_euclidean_distances(np.array(S))
-print(str(arr_scaler(distances)))
+logging.info('Points:\n{}'.format(str(S)))
+logging.info('Scaled distances:\n{}'.format(str(arr_scaler(distances))))
+# print(str(arr_scaler(distances)))
 c = 1
 
 h = Hierarchy(S, distances, c)
 
-print(str(h))
+# print(str(h))
